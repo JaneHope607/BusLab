5a099c30f8a32c36703293c0aa6a29095470346c
Jane O'Donnell <janehopeodonnell@gmail.com>
2020-08-25 14:10:19 +0100

    all tests passing for bus

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,17 +1,26 @@
 import java.util.ArrayList;
 
 public class Bus {
-
     private String destination;
     private int capacity;
     private ArrayList<Person> passengers;
 
-    public Bus() {
+    public Bus(String destination, int capacity) {
         this.passengers = new ArrayList<Person>();
+        this.destination = destination;
+        this.capacity = capacity;
     }
 
 
     public int getPassengers() {
         return this.passengers.size();
+    }
+
+    public void addPassenger(Person person) {
+        this.passengers.add(person);
+    }
+
+    public void removePassenger() {
+        this.passengers.remove(0);
     }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -5,14 +5,29 @@ import static org.junit.Assert.assertEquals;
 
 public class BusTest {
     private Bus bus;
+    private Person person;
 
     @Before
     public void before() {
-        bus = new Bus();
+        bus = new Bus("Edinburgh", 40);
+        person = new Person();
     }
 
     @Test
     public void hasNoPassengers() {
+        assertEquals(0, bus.getPassengers());
+    }
+
+    @Test
+    public void canAddPassenger() {
+        bus.addPassenger(person);
+        assertEquals(1, bus.getPassengers());
+    }
+
+    @Test
+    public void canRemovePassenger() {
+        bus.addPassenger(person);
+        bus.removePassenger();
         assertEquals(0, bus.getPassengers());
     }
 }
